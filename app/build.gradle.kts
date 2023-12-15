5cc45d1ff272b49f11ef1de10a4ddff0164b14bc
Parth Thakar <parththakar59@gmail.com>
2023-12-15 13:13:30 +0530

    Solved Dependency issues with kts and mapbox.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,11 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    implementation ("com.mapbox.extension:maps-compose:0.1.0")
+    // Pick your versions of Android Mapbox Map SDK (compatible with Maps SDK v11.0+.)
+    implementation ("com.mapbox.maps:android:11.0.0-beta.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +71,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,18 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                username = ("mapbox")
+                password = ("INSERT_MAPBOX_ACCESS_TOKEN_HERE")
+            }
+        }
     }
-}
 
-rootProject.name = "mapbox_compose"
-include(":app")
- 
+    rootProject.name = "mapbox_compose"
+    include(":app")
+}
